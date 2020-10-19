90052b158e92e92ef7f3971ac49a298f1a6c13d4
Florencia Fernandez Bugna <florencia.bfernandez@mercadolibre.com>
2020-10-19 19:30:48 -0300

    se agregan ; y cierre de etiqueta p

--- ejercicio-16.js
+++ ejercicio-16.js
@@ -1,28 +1,28 @@
 'use strict'
 
 var numero = parseInt(prompt('Ingrese un número'));
-var resultado = ''
+var resultado = '';
 
 if (numero % 3 == 0) {
 
 	if (numero % 5 == 0) {
-		resultado += `<p>El número ${numero} es múltiplo de 3 y de 5 </p>`
-		console.log("primero")
+		resultado += `<p>El número ${numero} es múltiplo de 3 y de 5 </p>`;
+		console.log("primero");
 
 	} else {
-		resultado += `<p>El número ${numero} es múltiplo solo de 3 y no de 5`
-		console.log("segundo")
+		resultado += `<p>El número ${numero} es múltiplo solo de 3 y no de 5</p>`;
+		console.log("segundo");
 	}
 
 } else {
 
 	if (numero % 5 == 0) {
-		resultado += `<p>El número ${numero} es múltiplo unicamente de 5`
-		console.log("tres")
+		resultado += `<p>El número ${numero} es múltiplo unicamente de 5</p>`;
+		console.log("tres");
 	} else {
-		resultado += `<p>El número ${numero} no es múltiplo 3 ni de 5`
+		resultado += `<p>El número ${numero} no es múltiplo 3 ni de 5</p>`;
 	}
 	
-}
+};
 
-document.write(resultado)
+document.write(resultado);
